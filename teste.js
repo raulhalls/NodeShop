895b22e2a6899fadfaf0ab1feadc66969e19b4f6
halls <raulcardoso888@gmail.com>
2023-11-04 07:08:25 -0300

    vai ate pag 2

--- teste.js
+++ teste.js
@@ -3,16 +3,13 @@ const cherio = require('cherio');
 const request = require('request');
 
 const list = [];
-
-
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 const url = 'https://www.amazon.com.br';
 const searchfor = 'Livros';
-
+const urlLivros = 'https://www.amazon.com.br/s?k=livros&__mk_pt_BR=ÅMÅŽÕÑ&crid=2X3H5JI3OGIRP&qid=1699089360&sprefix=livro%2Caps%2C277&ref=sr_pg_1';
 let c = 1;
 
 (async () => {
-
     const browser = await pup.launch({headless: false, args:["--no-sandbox",'--disable-setuid-sandbox']});
     const page = await browser.newPage();
     console.log('iniciei');
@@ -22,50 +19,48 @@ let c = 1;
     
     await page.waitForSelector('#twotabsearchtextbox');//campo de pesquisa
     await page.type('#twotabsearchtextbox' , searchfor);
-
     console.log('pesquisei');
-
     
     await Promise.all([
-        
         page.waitForNavigation(),
         page.click('#nav-search-submit-button')
-
     ]);
 
-  const links =  await page.$$eval('.a-size-mini > a' , el => el.map(link => link.href));
+    const linksLoop = async function () {
 
-  console.log(links);
+        const links =  await page.$$eval('.a-size-mini > a' , el => el.map(link => link.href));
+        console.log(links);
 
-for(const link of links){
+        for(const link of links){
 
-    await page.goto(link);
-    //await page.waitForSelector();
+            await page.goto(link);
+            //await page.waitForSelector();
+            const title = await page.$eval('#productTitle' , element => element.innerText);
+            // const linkImg = await page.$eval('Seletor' , element => element.innerText);
+            const obj = {};
+            obj.title = title;
+            list.push(obj);
+            c++;
+            console.log('pagina' , c);
+        }
+        console.log(list);
+    }
     
-    const title = await page.$eval('#productTitle' , element => element.innerText);
-    // const linkImg = await page.$eval('Seletor' , element => element.innerText);
+    await linksLoop(); 
 
-    const obj = {};
-    obj.title = title;
+    await page.goto(urlLivros); 
 
-    list.push(obj);
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click('.s-pagination-next')
+    ]);
 
-    c++;
-    console.log('pagina' , c);
-}
-
-console.log(list);
+    console.log('pagina 2 de compras');
 
 
+    (async () => {
+        await sleep(9000);
+    })();
 
-
-(async () => {
-  console.log(new Date().getSeconds());
-  await sleep(9000);
-  console.log(new Date().getSeconds());
-})();
-   await browser.close();
-
-    
-
+    await browser.close();
 })();
